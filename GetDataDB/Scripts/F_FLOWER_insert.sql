f7b8304ae41314ffc3577bc2bb49bc0d6572b456
Pavel Foltyn <pavel.foltyn@volny.cz>
2023-04-21 03:45:35 +0200

    CHANGE Add more flowers to the SQL script to populate the db table.

--- GetDataDB/Scripts/F_FLOWER_insert.sql
+++ GetDataDB/Scripts/F_FLOWER_insert.sql
@@ -43,3 +43,33 @@ INSERT INTO
 
 
 
+INSERT INTO
+  F_FLOWER
+  (
+    -- ID,
+    SPECIES,
+    PREVAILING_COLOR
+  )
+  SELECT
+    -- 4             AS ID,
+    N'Sunflower'  AS SPECIES,
+    'Orange'      AS PREVAILING_COLOR
+;
+
+
+
+INSERT INTO
+  F_FLOWER
+  (
+    -- ID,
+    SPECIES,
+    PREVAILING_COLOR
+  )
+  SELECT
+    -- 5             AS ID,
+    N'Tulip'      AS SPECIES,
+    'Yellow'      AS PREVAILING_COLOR
+;
+
+
+
